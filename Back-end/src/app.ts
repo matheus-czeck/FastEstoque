eb19a3d22ece730b96024345c72d71e2b96d8497
matheus-czeck <matheusczeckcz@gmail.com>
2026-07-09 22:08:29 -0300

    fix: adicionando process em app.ts ajustando cors

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,14 +1,19 @@
+import cors from "cors";
 import express from "express";
-import cors from 'cors'
 import AuthRoutes from "./routes/auth.routes";
 import ProdutoRoutes from "./routes/produto.routes";
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS?.split(",") || [
+      "http://localhost:4200",
+    ],
+  }),
+);
 
-app.use("/auth", AuthRoutes)
-app.use("/products", ProdutoRoutes)
-
+app.use("/auth", AuthRoutes);
+app.use("/products", ProdutoRoutes);
 
 export default app;
